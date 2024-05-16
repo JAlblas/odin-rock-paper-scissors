ab1769629e3a6226e291c9a9f585fe5ab7ec3039
Jasper Alblas <just_jasper@hotmail.com>
2024-05-16 22:13:39 +0200

    Adds playgame function to call playround 5 times

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
-function playRound(humanChoice, computerChoice) {
-    switch (humanChoice) {
-        case "rock":
-            if (computerChoice == "scissors") {
-                humanScore++;
-            } else if (computerChoice == "paper") {
-                computerScore++;
-            }
-            break;
-        case "paper":
-            if (computerChoice == "scissors") {
-                computerScore++;
-            } else if (computerChoice == "rock") {
-                humanScore++;
-            }
-            break;
-        case "scissors":
-            if (computerChoice == "rock") {
-                computerChoice++;
-            } else if (computerChoice == "paper") {
-                humanScore++;
-            }
-            break;
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+        switch (humanChoice) {
+            case "rock":
+                if (computerChoice == "scissors") {
+                    humanScore++;
+                } else if (computerChoice == "paper") {
+                    computerScore++;
+                }
+                break;
+            case "paper":
+                if (computerChoice == "scissors") {
+                    computerScore++;
+                } else if (computerChoice == "rock") {
+                    humanScore++;
+                }
+                break;
+            case "scissors":
+                if (computerChoice == "rock") {
+                    computerChoice++;
+                } else if (computerChoice == "paper") {
+                    humanScore++;
+                }
+                break;
+        }
+        console.log(`${humanChoice} VS ${computerChoice}`);
+        console.log(humanScore);
+        console.log(computerScore);
     }
-    console.log(`${humanChoice} VS ${computerChoice}`);
-    console.log(humanScore);
-    console.log(computerScore);
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("YOU WIN!");
+    } else if (humanScore < computerScore) {
+        console.log("YOU LOSE!");
+    } else {
+        console.log("DRAW!");
+    }
 }
 
 function getComputerChoice() {
@@ -47,13 +68,7 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
 
 
 
