a9f29381238b92dbb9c55d759b9c3eb5bf67a75f
Jasper Alblas <just_jasper@hotmail.com>
2024-05-16 21:50:39 +0200

    Implements human choice function

--- script.js
+++ script.js
@@ -4,4 +4,19 @@ function getComputerChoice() {
     return POSSIBILITIES[Math.floor(Math.random() * 3)];
 }
 
+function getHumanChoice() {
+    while (true) {
+        choice = prompt("Rock, paper, scissors?");
+        if (choice === null) {
+            // User cancelled the prompt
+            break;
+        }
+        choice = choice.toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        }
+    }
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
